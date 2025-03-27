59c8a53d20bf9e3a1a44253ca53ac61946f6d66f
El-Ayadi-Rida <rdlzaar@gmail.com>
2025-03-27 03:25:08 +0000

    FOOTER

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -15,18 +15,18 @@ const Footer = () => {
         <Container>
           <Row>
             <Col xs="12" sm="6">
-              <p className="mb-0 text-muted text-medium">Colored Strategies 2021</p>
+              <p className="mb-0 text-muted text-medium"> PFE HOTEL MANAGMENT 2025</p>
             </Col>
             <Col sm="6" className="d-none d-sm-block">
               <Breadcrumb className="pt-0 pe-0 mb-0 float-end">
                 <Breadcrumb.Item className="mb-0 text-medium" href="#/" linkProps={{ className: 'btn-link' }}>
-                  Review
+                  DOUAE
                 </Breadcrumb.Item>
                 <Breadcrumb.Item className="mb-0 text-medium" href="#/" linkProps={{ className: 'btn-link' }}>
-                  Purchase
+                  IMANE
                 </Breadcrumb.Item>
                 <Breadcrumb.Item className="mb-0 text-medium" href="#/" linkProps={{ className: 'btn-link' }}>
-                  Docs
+                  PROF: Ibtissam TOUAHRI
                 </Breadcrumb.Item>
               </Breadcrumb>
             </Col>
